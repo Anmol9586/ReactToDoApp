157ea34bf75aba4d1a4e28f48ff9521571b2a710
Anmol9586 <95464814+Anmol9586@users.noreply.github.com>
2024-07-06 01:12:02 +0530

    Update firebase.js

--- TooDoo/src/firebase/firebase.js
+++ TooDoo/src/firebase/firebase.js
@@ -3,18 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnjroTIEiDMR8is__bcgQsqqb-aHbQYAI",
-  authDomain: "todo-app-52869.firebaseapp.com",
-  projectId: "todo-app-52869",
-  storageBucket: "todo-app-52869.appspot.com",
-  messagingSenderId: "1077805568336",
-  appId: "1:1077805568336:web:cf197747dce6e642d19c55"
+  apiKey: "AIzaSyBenCZVyGl7i886z1wYwAUw1-hmDGcMv_w",
+  authDomain: "todo-app-17de6.firebaseapp.com",
+  projectId: "todo-app-17de6",
+  storageBucket: "todo-app-17de6.appspot.com",
+  messagingSenderId: "589415912189",
+  appId: "1:589415912189:web:d62fa0eb7ec17666736494"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-
-
 export { app, auth , db};
